cbe04e49370d60d87839203bcb3fef52f9abfaeb
Ryu Sato <ryu@weseek.co.jp>
2019-06-23 03:12:32 +0900

    initialize rails and generate models

---
+++ app/models/author.rb
@@ -0,0 +1,5 @@
+class Author < ApplicationRecord
+  def to_s
+    name
+  end
+end
---
+++ db/migrate/20190622175719_create_books.rb
@@ -0,0 +1,11 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.string :name
+      t.datetime :published_at
+      t.references :author, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
